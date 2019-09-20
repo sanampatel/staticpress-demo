6222703525442014b54c26c3631001812c863602
hiral <hiral@allindiainvest.com>
2019-09-20 16:09:15 +0530

    cateogy css added

--- source/_partials/sidebar.blade.php
+++ source/_partials/sidebar.blade.php
@@ -8,7 +8,7 @@
 		<h3 class="heading-title"><span class="heading-title-span uk-text-bold">Category</span><span class="heading-title-divider"></span></h3>
 		<ul class="uk-list">
 			@foreach ($categories as $category)
-			<li class="uk-padding-small" style="background: url({{ $page->mainUrl }}/images/{{ basename($category->image) }});">
+			<li class="uk-padding-small list-cat" style="background: url({{ $page->mainUrl }}/images/{{ basename($category->image) }});">
 				<a class="cat light" href="/categories/{{ $category->title }}" title="View posts in {{ $category->title }}">{{ ucwords($category->title) }}</a>
 			</li>
 			@endforeach	
@@ -20,9 +20,6 @@
 	<div class="uk-margin-medium-top">
 		<h3 class="heading-title"><span class="heading-title-span uk-text-bold">Tag</span><span class="heading-title-divider"></span></h3>
 		@foreach ($tags as $tag)
-{{-- 			<span class="uk-badge">
-				<a class="tag light uk-padding-small" href="/tags/{{ $tag->title }}" title="View posts in {{ $tag->title }}">{{ ucwords($tag->title) }}</a>
-			</span> --}}
 			<span class="uk-label">
 				<a class="tag light uk-padding-small" href="/tags/{{ $tag->title }}" title="View posts in {{ $tag->title }}">{{ ucwords($tag->title) }}</a>
 			</span>
