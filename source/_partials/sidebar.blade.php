63654abe1c0bbee01c14b55b586b3f4383bfefcb
hiral <hiral@allindiainvest.com>
2019-09-20 16:02:59 +0530

    title size changed in index page

--- source/_partials/sidebar.blade.php
+++ source/_partials/sidebar.blade.php
@@ -1,11 +1,11 @@
 <div class="uk-margin">
-	<h3 class="heading-title"><span class="heading-title-span">About Author</span><span class="heading-title-divider"></span></h3>
+	<h3 class="heading-title"><span class="heading-title-span uk-text-bold">About Author</span><span class="heading-title-divider"></span></h3>
 	<p>{{ $settings->setting->aboutauthor }}</p>
 </div>
 
 @if ($categories)
 	<div class="uk-margin-medium-top">
-		<h3 class="heading-title"><span class="heading-title-span">Category</span><span class="heading-title-divider"></span></h3>
+		<h3 class="heading-title"><span class="heading-title-span uk-text-bold">Category</span><span class="heading-title-divider"></span></h3>
 		<ul class="uk-list">
 			@foreach ($categories as $category)
 			<li class="uk-padding-small" style="background: url({{ $page->mainUrl }}/images/{{ basename($category->image) }});">
@@ -18,9 +18,12 @@
 
 @if ($tags)
 	<div class="uk-margin-medium-top">
-		<h3 class="heading-title"><span class="heading-title-span">Tag</span><span class="heading-title-divider"></span></h3>
+		<h3 class="heading-title"><span class="heading-title-span uk-text-bold">Tag</span><span class="heading-title-divider"></span></h3>
 		@foreach ($tags as $tag)
-			<span class="uk-badge">
+{{-- 			<span class="uk-badge">
+				<a class="tag light uk-padding-small" href="/tags/{{ $tag->title }}" title="View posts in {{ $tag->title }}">{{ ucwords($tag->title) }}</a>
+			</span> --}}
+			<span class="uk-label">
 				<a class="tag light uk-padding-small" href="/tags/{{ $tag->title }}" title="View posts in {{ $tag->title }}">{{ ucwords($tag->title) }}</a>
 			</span>
 		@endforeach
